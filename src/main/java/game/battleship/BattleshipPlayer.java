5f4973811927bfe3b2dcae1ab2106d80f682ac47
gpuget <gpuget@excilys.com>
2021-02-06 23:44:40 +0100

    Battleship tests with scanner

--- src/main/java/game/battleship/BattleshipPlayer.java
+++ src/main/java/game/battleship/BattleshipPlayer.java
@@ -2,6 +2,7 @@ package game.battleship;
 
 import game.Player;
 import game.battleship.board.Board;
+import java.util.Objects;
 
 public class BattleshipPlayer extends Player {
   private final Board board;
@@ -9,5 +10,33 @@ public class BattleshipPlayer extends Player {
   public BattleshipPlayer(String name, Board board) {
     super(name);
     this.board = board;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    BattleshipPlayer that = (BattleshipPlayer) o;
+    return Objects.equals(board, that.board);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), board);
+  }
+
+  @Override
+  public String toString() {
+    return "BattleshipPlayer{" +
+        "name=" + getName() + ", " +
+        "ships=" + board.getShips() +
+        '}';
   }
 }
--- src/test/java/game/battleship/BattleshipTest.java
+++ src/test/java/game/battleship/BattleshipTest.java
@@ -3,9 +3,29 @@ package game.battleship;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import game.battleship.board.Board;
+import game.battleship.board.Spot;
+import game.battleship.ship.Ship;
+import game.battleship.ship.ShipType;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BattleshipTest {
+  private static final Ship CARRIER = new Ship(ShipType.CARRIER, List.of(
+      new Spot(0, 0),
+      new Spot(1, 0),
+      new Spot(2, 0),
+      new Spot(3, 0),
+      new Spot(4, 0)
+  ));
+
+  @BeforeEach
+  void setUp() {
+    System.setIn(new BufferedInputStream(new ByteArrayInputStream("A1D\nA1D\n".getBytes())));
+  }
+
   @Test
   void whenCreate_thenOk() {
     Battleship battleship = new Battleship();
@@ -19,20 +39,24 @@ class BattleshipTest {
   @Test
   void whenCreatePlayer_thenPlayer1() {
     Battleship battleship = new Battleship();
+    Board board = Board.empty();
+    board.places(CARRIER);
 
     BattleshipPlayer player1 = battleship.createPlayer();
 
-    assertThat(player1).isEqualTo(new BattleshipPlayer("Player 1", Board.empty()));
+    assertThat(player1).isEqualTo(new BattleshipPlayer("Player 1", board));
   }
 
   @Test
   void givenOnePlayerInGame_whenNewPlayer_then2Players() {
     Battleship battleship = new Battleship();
     BattleshipPlayer player1 = battleship.newPlayer();
+    Board board = Board.empty();
+    board.places(CARRIER);
 
     BattleshipPlayer player2 = battleship.newPlayer();
 
     assertThat(battleship.getPlayers()).hasSize(2);
-    assertThat(battleship.getPlayers().get(1)).isEqualTo(new BattleshipPlayer("Player 2", Board.empty()));
+    assertThat(battleship.getPlayers().get(1)).isEqualTo(new BattleshipPlayer("Player 2", board));
   }
 }
