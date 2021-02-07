80785bde4d722a0c9fb423fa0fdbe49ff9d11f55
gpuget <gpuget@excilys.com>
2021-02-07 22:38:30 +0100

    Refactoring

--- src/main/java/game/GameFactory.java
+++ src/main/java/game/GameFactory.java
@@ -5,13 +5,13 @@ import java.util.function.Supplier;
 public enum GameFactory {
   ;
 
-  public final Supplier<Game> gameSupplier;
+  public final Supplier<GenericGame> gameSupplier;
 
-  GameFactory(Supplier<Game> gameSupplier) {
+  GameFactory(Supplier<GenericGame> gameSupplier) {
     this.gameSupplier = gameSupplier;
   }
 
-  public Game create() {
+  public GenericGame create() {
     return this.gameSupplier.get();
   }
 }
